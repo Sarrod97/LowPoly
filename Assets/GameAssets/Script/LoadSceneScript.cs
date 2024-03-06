c27e7991f6111c2c5e4ac690c6fe9674503e4d7a
Sarrod97 <r.puerta.sara@gmail.com>
2024-03-06 20:21:35 +0100

    Initial commit

---
+++ Assets/GameAssets/Script/LoadSceneScript.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneScript : MonoBehaviour
+{
+   public void teleport ()
+   {
+
+    //Para cagar la escena
+    SceneManager.LoadScene ("Exterior");
+    
+   }
+}
