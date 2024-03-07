04b23d1ae87fec0bbe4ba0ca1936aa2aa47c638c
Sarrod97 <r.puerta.sara@gmail.com>
2024-03-07 19:14:58 +0100

    cambios 07.03.24.4

---
+++ Assets/GameAssets/Script/Llave.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Llave : MonoBehaviour
+{
+    // Para que rote la llave
+    public float speed;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * speed);    
+    }
+}
