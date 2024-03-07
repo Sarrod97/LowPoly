da9fb6f281bed857e5eadb55539beee2d03acbc5
Sarrod97 <r.puerta.sara@gmail.com>
2024-03-07 16:44:40 +0100

    cambio 07.03.24.2

--- Assets/GameAssets/Script/LoadSceneScript.cs
+++ Assets/GameAssets/Script/LoadSceneScript.cs
@@ -4,11 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class LoadSceneScript : MonoBehaviour
 {
-   public void teleport ()
+   public void Start ()
    {
 
     //Para cagar la escena
     SceneManager.LoadScene ("Exterior");
+    
+   }
+
+   public void Exit ()
+   {
+      //Para salir del juego
+    
+        Application.Quit();
     
    }
 }
